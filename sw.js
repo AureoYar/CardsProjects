764b2c456ca6d30f91765f202174234d1771b8c4
AureoYar <aureoyar@gmail.com>
2025-12-16 23:35:35 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'insight-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
